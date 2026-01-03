74208898613625242239a5e8ec82938c651860fc
tgarda <tgarda@hotmail.com>
2026-01-03 01:09:02 -0800

    updated echo_server math_server #3

--- echo_server.py
+++ echo_server.py
@@ -3,15 +3,27 @@ from mcp.server.transport_security import TransportSecuritySettings
 
 # mcp = FastMCP(name="EchoServer", stateless_http=True)
 
+# REF: https://github.com/modelcontextprotocol/python-sdk/issues/1798
+# THIS DID NOT WORK:
+# mcp = FastMCP(
+#     "EchoServer",
+#     stateless_http=True,
+#     transport_security=TransportSecuritySettings(
+#         enable_dns_rebinding_protection=True,
+#         allowed_hosts=["localhost:*", "127.0.0.1:*", "mcp01.onrender.com:*"],
+#         allowed_origins=["http://localhost:*", "http://mcp01.onrender.com:*"],
+#     )
+# )
+
 mcp = FastMCP(
     "EchoServer",
     stateless_http=True,
     transport_security=TransportSecuritySettings(
-        enable_dns_rebinding_protection=True,
-        allowed_hosts=["localhost:*", "127.0.0.1:*", "mcp01.onrender.com:*"],
-        allowed_origins=["http://localhost:*", "http://mcp01.onrender.com:*"],
+        enable_dns_rebinding_protection=False,
     )
 )
+
+
 
 @mcp.tool(description="A simple echo tool")
 def echo(message: str) -> str:
