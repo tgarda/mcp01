12e1c8e7554df183fe4c730e18e44e18afb30ff2
tgarda <tgarda@hotmail.com>
2026-01-03 00:27:21 -0800

    updated echo_server math_server #1

--- echo_server.py
+++ echo_server.py
@@ -1,6 +1,16 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP(name="EchoServer", stateless_http=True)
+# mcp = FastMCP(name="EchoServer", stateless_http=True)
+
+mcp = FastMCP(
+    "EchoServer",
+    stateless_http=True,
+    transport_security=TransportSecuritySettings(
+        enable_dns_rebinding_protection=True,
+        allowed_hosts=["localhost:*", "127.0.0.1:*", "mcp01.onrender.com:*"],
+        allowed_origins=["http://localhost:*", "http://mcp01.onrender.com:*"],
+    )
+)
 
 @mcp.tool(description="A simple echo tool")
 def echo(message: str) -> str:
--- math_server.py
+++ math_server.py
@@ -1,6 +1,17 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.transport_security import TransportSecuritySettings
 
-mcp = FastMCP(name="MathServer", stateless_http=True)
+# mcp = FastMCP(name="MathServer", stateless_http=True)
+
+mcp = FastMCP(
+    "MathServer",
+    stateless_http=True,
+    transport_security=TransportSecuritySettings(
+        enable_dns_rebinding_protection=True,
+        allowed_hosts=["localhost:*", "127.0.0.1:*", "mcp01.onrender.com:*"],
+        allowed_origins=["http://localhost:*", "http://mcp01.onrender.com:*"],
+    )
+)
 
 @mcp.tool(description="A simple add tool")
 def add_two(n: int) -> int:
